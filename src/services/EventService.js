467c3bc025b663ac99dce1341ca5c31632be467c
Timothy <62649205+krakkajack@users.noreply.github.com>
2021-08-22 18:27:33 +1000

    Update database

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://my-json-server.typicode.com/krakkajack/real-world_vue-3",
+  baseURL: "https://my-json-server.typicode.com/krakkajack/Eventcards",
   withCredentials: false,
   headers: {
     Accept: "application/json",
